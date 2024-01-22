3b09ed58c9e6bd0060ceee2d383de1aee4df16c4
Dark <anisri477@gmail.com>
2024-01-22 17:23:17 +0530

    update on performance

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ def load_mnist():
 def with_lstm():
     X_train, y_train, X_test, y_test = load_mnist()
 
-    max_neurons = 10
-    threshold_epochs = 3
+    max_neurons = 40
+    threshold_epochs = 6
     X_train_lstm = X_train.reshape((X_train.shape[0], 1, -1))
     X_test_lstm = X_test.reshape((X_test.shape[0], 1, -1))
 
@@ -33,7 +33,7 @@ def with_lstm():
         X_train_lstm.shape[2], y_train.shape[1], 10, 5, max_neurons, threshold_epochs)
     start_time = time.time()
     history_with_lstm = architecture_with.train(
-        X_train_lstm, y_train, epochs=10)
+        X_train_lstm, y_train, epochs=30)
     end_time = time.time()
     pred = architecture_with.predict(X_test_lstm)
     acc = accuracy_score(np.argmax(y_test, axis=1), np.argmax(pred, axis=1))
@@ -43,15 +43,15 @@ def with_lstm():
 def without_lstm():
     X_train, y_train, X_test, y_test = load_mnist()
 
-    max_neurons = 10
-    threshold_epochs = 3
+    max_neurons = 12
+    threshold_epochs = 4
     X_train_no_lstm = X_train.reshape((X_train.shape[0], -1))
     X_test_no_lstm = X_test.reshape((X_test.shape[0], -1))
 
     architecture_without = NeuroFuzzyNetwork_(
         X_train_no_lstm.shape[1], y_train.shape[1], 10, 5,max_neurons, threshold_epochs)
     history_without_lstm = architecture_without.train(
-        X_train_no_lstm, y_train, epochs=10)
+        X_train_no_lstm, y_train, epochs=20)
 
     start_time = time.time()
     pred = architecture_without.predict(X_test_no_lstm)
@@ -66,7 +66,7 @@ def normal():
     model.add(Dense(10,activation='relu',input_shape=(784,)))
     model.add(Dense(10,activation='softmax'))
     model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
-    history = model.fit(X_train,y_train,epochs=10,batch_size=32,verbose=0)
+    history = model.fit(X_train,y_train,epochs=20,batch_size=32,verbose=0)
     start_time = time.time()
     pred = model.predict(X_test)
     end_time = time.time()
@@ -74,4 +74,21 @@ def normal():
     return model,acc,end_time-start_time,history
 
 
-
+def cnn():
+    X_train,y_train,X_test,y_test = load_mnist()
+    X_train = X_train.reshape((X_train.shape[0],28,28,1))
+    X_test = X_test.reshape((X_test.shape[0],28,28,1))
+    model = Sequential()
+    model.add(tf.keras.layers.Conv2D(32,(3,3),activation='relu',input_shape=(28,28,1)))
+    model.add(tf.keras.layers.MaxPooling2D((2,2)))
+    model.add(tf.keras.layers.Conv2D(64,(3,3),activation='relu'))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(64,activation='relu'))
+    model.add(tf.keras.layers.Dense(10,activation='softmax'))
+    model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    history = model.fit(X_train,y_train,epochs=20,batch_size=32,verbose=0)
+    start_time = time.time()
+    pred = model.predict(X_test)
+    end_time = time.time()
+    acc = accuracy_score(np.argmax(y_test,axis=1),np.argmax(pred,axis=1))
+    return model,acc,end_time-start_time,history
